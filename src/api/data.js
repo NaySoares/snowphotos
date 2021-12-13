f3879acfd98cca8f147d29ab17da13ecb2455b45
Elienai <elienay.soares07@gmail.com>
2021-12-13 11:46:23 -0300

    add(api): fake data

---
+++ src/api/data.js
@@ -0,0 +1,53 @@
+import elaina from '../assets/images/elaina.png'
+import shadow from '../assets/images/shadow.png'
+import goblin from '../assets/images/goblin.png'
+import heroi from '../assets/images/heroiRealista.png'
+import sameDream from '../assets/images/sameDream.jpg'
+import jkHaru from '../assets/images/jkHaru.png'
+import nobunaga from '../assets/images/nobunaga.png'
+export const DataImg = [
+  {
+    title: 'Elaina',
+    src: elaina,
+    description: 'Bruxa Errante: A Jornada de Elaina',
+    size: 40
+  },
+  {
+    title: 'SameDream',
+    src: sameDream,
+    description: 'Tive Aquele Mesmo Sonho de Novo',
+    size: 18
+  },
+  {
+    title: 'Shadow',
+    src: shadow,
+    description: 'A Eminência nas Sombras',
+    size: 40
+  },
+  {
+    title: 'Goblin',
+    src: goblin,
+    description: 'Matador de Goblins',
+    size: 40
+  },
+  {
+    title: 'Heroi',
+    src: heroi,
+    description: 'Como um Herói Realista Reconstruiu o Reino',
+    size: 40
+  },
+  {
+    title: 'JkHaru',
+    src: jkHaru,
+    description: 'JK Haru é uma Profissional do Sexo em Outro Mundo',
+    size: 40
+  },
+  {
+    title: 'Nobunaga',
+    src: nobunaga,
+    description: 'Um Misterioso Trabalho Chamado Nobunaga',
+    size: 40
+  },
+]
+
+export default DataImg
