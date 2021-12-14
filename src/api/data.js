2abea7e8eae5530a78837653201279b9600203e1
Elienai <elienay.soares07@gmail.com>
2021-12-14 15:23:21 -0300

    fix(data): add imgs modal

--- src/api/data.js
+++ src/api/data.js
@@ -1,53 +1,81 @@
-import elaina from '../assets/images/elaina.png'
-import shadow from '../assets/images/shadow.png'
-import goblin from '../assets/images/goblin.png'
-import heroi from '../assets/images/heroiRealista.png'
-import sameDream from '../assets/images/sameDream.jpg'
-import jkHaru from '../assets/images/jkHaru.png'
-import nobunaga from '../assets/images/nobunaga.png'
+import elaina from "../assets/images/elaina.png";
+import elainaModal from "../assets/images/elaina2.jpg";
+import shadow from "../assets/images/shadow.png";
+import shadowModal from "../assets/images/shadow2.jpg";
+import goblin from "../assets/images/goblin.png";
+import goblinModal from "../assets/images/goblin2.jpg";
+import heroi from "../assets/images/heroiRealista.png";
+import heroiModal from "../assets/images/heroiRealista2.jpg";
+import sameDream from "../assets/images/sameDream.jpg";
+import sameDreamModal from "../assets/images/sameDream2.jpg";
+import jkHaru from "../assets/images/jkHaru.png";
+import jkHaruModal from "../assets/images/jkHaru2.jpg";
+import nobunaga from "../assets/images/nobunaga.png";
+import nobunagaModal from "../assets/images/nobunaga2.jpg";
+
 export const DataImg = [
   {
-    title: 'Elaina',
+    id: 1,
+    title: "Bruxa Errante: A Jornada de Elaina",
     src: elaina,
-    description: 'Bruxa Errante: A Jornada de Elaina',
-    size: 40
+    srcModal: elainaModal,
+    description: `Essa capa foi uma das primeiras que fiz quando comecei a editar capas
+    de Light Novels, obviamente não foi nada fácil recriar o titulo com os
+    aspectos originais, em particular, os "ramos" que passam atrás do
+    nome. Um ponto que fica como curiosidade é que o sub-titulo "A Jornada
+    de Elaina" está com uma fonte diferente da original, depois de vários
+    meses eu consegui a fonte original mas ainda não tive tempo de
+    atualizar essa arte.`,
+    size: 40,
   },
   {
-    title: 'SameDream',
+    id: 2,
+    title: "Tive Aquele Mesmo Sonho de Novo",
     src: sameDream,
-    description: 'Tive Aquele Mesmo Sonho de Novo',
-    size: 18
+    srcModal: sameDreamModal,
+    description: "Tive Aquele Mesmo Sonho de Novo",
+    size: 18,
   },
   {
-    title: 'Shadow',
+    id: 3,
+    title: "A Eminência nas Sombras",
     src: shadow,
-    description: 'A Eminência nas Sombras',
-    size: 40
+    srcModal: shadowModal,
+    description: "A Eminência nas Sombras",
+    size: 40,
   },
   {
-    title: 'Goblin',
+    id: 4,
+    title: "Matador de Goblins",
     src: goblin,
-    description: 'Matador de Goblins',
-    size: 40
+    srcModal: goblinModal,
+    description: "Matador de Goblins",
+    size: 40,
   },
   {
-    title: 'Heroi',
+    id: 5,
+    title: "Como um Herói Realista Reconstruiu o Reino",
     src: heroi,
-    description: 'Como um Herói Realista Reconstruiu o Reino',
-    size: 40
+    srcModal: heroiModal,
+    description: "Como um Herói Realista Reconstruiu o Reino",
+    size: 40,
   },
   {
-    title: 'JkHaru',
+    id: 6,
+    title: "JK Haru é uma Profissional do Sexo em Outro Mundo",
     src: jkHaru,
-    description: 'JK Haru é uma Profissional do Sexo em Outro Mundo',
-    size: 40
+    srcModal: jkHaruModal,
+    description: "JK Haru é uma Profissional do Sexo em Outro Mundo",
+    size: 40,
   },
   {
-    title: 'Nobunaga',
+    id: 7,
+    title: "Um Misterioso Trabalho Chamado Nobunaga",
     src: nobunaga,
-    description: 'Um Misterioso Trabalho Chamado Nobunaga',
-    size: 40
+    srcModal: nobunagaModal,
+    description: "Um Misterioso Trabalho Chamado Nobunaga",
+    size: 40,
   },
-]
+];
 
-export default DataImg
+export default DataImg;
